4808f87e19601ea4ddb2c9a6c639a078d3a6ce45
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 10:01:59 +0100

    träning om output med mening och Document Write

--- main.js
+++ main.js
@@ -23,9 +23,17 @@ var myName = "Abdallah",
 alert("Hello My Name Is " + myName + " And My Age Is " + myAge);
 */
 
+/*
 var myName = "Abdallah",
 
     myAge = 30;
 
 //alert("Hello My Name Is " + myName + " And My Age Is " + myAge);
-document.write("Hello My Name Is " + myName + " And My Age Is " + myAge)
+//document.write("Hello My Name Is " + myName + " And My Age Is " + myAge)
+*/
+
+var myName = "Abdallah",
+
+    myAge = 30;
+
+document.getElementByTd("test").innerHTML = "Hello My Name Is " + myName + " And My Age Is " + myAge;
